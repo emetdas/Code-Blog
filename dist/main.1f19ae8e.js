355f44d0dd43d56874c239c15a61320b8cdc7e11
emetdas <emet@b2bleadstaff.com>
2021-05-24 22:50:29 +0600

    update

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -122,7 +122,7 @@ var name = "Emet Das";
 console.log(name);
 
 var myFun = function myFun() {
-  console.log("Emet Das Developer ");
+  console.log("Emet Das  Developer ");
 };
 
 myFun();
@@ -163,7 +163,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57095" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57731" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
