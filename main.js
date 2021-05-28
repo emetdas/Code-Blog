673fc2abad25c7a281fe6554887ab81d1e49529d
emetdas <emet@b2bleadstaff.com>
2021-05-29 00:42:39 +0600

    update

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-let name = "Emet Das";
+let name = 'Emet Das';
 console.log(name);
 
 let myFun = () => {
-    console.log("Emet Das  Developer ");
-}
+  console.log('Emet Das  Developer ');
+};
 myFun();
 console.log(`My Name is ${name}`);
-let array = ["Emet", "Das"];
+let array = ['Emet', 'Das'];
 array.forEach((e) => {
-    console.log(e);
+  console.log(e);
 });
 
 
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept();
 }
